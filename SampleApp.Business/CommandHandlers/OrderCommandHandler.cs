90b914822b93e590f219cadf388ecd9718240c71
khavo <vonguyen.anhkha@gmail.com>
2018-03-27 22:47:05 +0700

    Add new inital class for Business Domain

--- SampleApp.Business/CommandHandlers/OrderCommandHandler.cs
+++ SampleApp.Business/CommandHandlers/OrderCommandHandler.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SampleApp.Business.Domains
+namespace SampleApp.Business.CommandHandlers
 {
-    public class Product
+    public class OrderCommandHandler
     {
     }
 }
