f6bbc8e86c35bc4bc208099010ff222d2862b8e5
ORIENTSOFTWARE\vnakha <vonguyen.anhkha@gmail.com>
2018-03-30 15:31:00 +0700

    Register module at Ordering

--- SampleApp.Api/Startup.cs
+++ SampleApp.Api/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SampleApp.Core;
 using SampleApp.Logging;
+using SampleApp.Ordering;
 
 namespace SampleApp.Api
 {
@@ -29,6 +30,7 @@ namespace SampleApp.Api
             var builder = new ContainerBuilder();
             builder.RegisterModule<CoreModule>();
             builder.RegisterModule<LoggingModule>();
+            builder.RegisterModule<OrderingModule>();
 
             builder.Populate(services);
 
--- SampleApp.Ordering/OrderingModule.cs
+++ SampleApp.Ordering/OrderingModule.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Autofac;
+using SampleApp.Ordering.CommandHandlers;
+using SampleApp.Ordering.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,13 @@ using System.Threading.Tasks;
 
 namespace SampleApp.Ordering
 {
-    public class OrderingModule
+    public class OrderingModule : Module
     {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+            builder.RegisterType<OrderCommandHandler>().As<IOrderCommandHandler>().InstancePerLifetimeScope();
+            builder.RegisterType<OrderService>().As<IOrderService>().InstancePerLifetimeScope();
+        }
     }
 }
