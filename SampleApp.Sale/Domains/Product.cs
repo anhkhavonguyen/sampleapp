cbe7a2f71eb571ae58d8568ad01a7ae68f4bb3e1
ORIENTSOFTWARE\vnakha <vonguyen.anhkha@gmail.com>
2018-04-11 08:54:16 +0700

    Add new domain bussiness : Sale

---
+++ SampleApp.Sale/Domains/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApp.Sale.Domains
+{
+    public class Product
+    {
+        public int Id { get; set; }
+    }
+}
