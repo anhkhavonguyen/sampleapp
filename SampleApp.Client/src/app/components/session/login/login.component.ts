a86b2555e1bda7c101074191f738bcd05e24899a
khavo <vonguyen.anhkha@gmail.com>
2018-04-19 23:34:11 +0700

    add new home, login component

--- SampleApp.Client/src/app/components/session/login/login.component.ts
+++ SampleApp.Client/src/app/components/session/login/login.component.ts
@@ -1,1 +1,10 @@
-﻿
+﻿import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'log-in',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+}
