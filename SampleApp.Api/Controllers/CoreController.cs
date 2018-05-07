4488395543b103b4c4c26f20b44918bff5f56d1f
ORIENTSOFTWARE\vnakha <vonguyen.anhkha@gmail.com>
2018-05-07 13:49:55 +0700

    Refactor

--- SampleApp.Api/Controllers/CoreController.cs
+++ SampleApp.Api/Controllers/CoreController.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using SampleApp.Core.DbContext;
 using SampleApp.Core.Messages;
 using SampleApp.Core.Services;
-using SampleApp.Core.UnitOfWork;
 
 namespace SampleApp.Api.Controllers
 {
